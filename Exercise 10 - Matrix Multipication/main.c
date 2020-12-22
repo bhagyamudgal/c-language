1f2839766604e711d2a60471ab5e8c00da42635c
Bhagya Mudgal <bhagyamudgal@gmail.com>
2020-12-22 18:18:20 +0530

    Exercise 10 Completed

--- Exercise 10 - Matrix Multipication/main.c
+++ Exercise 10 - Matrix Multipication/main.c
@@ -1,49 +1,57 @@
 #include <stdio.h>
 
-int rows1, coloumns1, rows2, coloumns2, sum = 0;
-int a[10][10];
-int b[10][10];
-int ab[10][10];
-
-void aInput()
+// function to get matrix elements entered by the user
+void getMatrixElements(int matrix[][10], int row, int column)
 {
-    for (int i = 1; i <= rows1; i++)
-        for (int j = 1; j <= coloumns1; j++)
-        {
-            printf("\nEnter Value for row %d and coloumn %d  : ", i, j);
-            scanf("%d", &a[i][j]);
-        }
-}
 
-void aOutput()
-{
-    for (int i = 1; i <= rows1; i++)
+    for (int i = 0; i < row; i++)
     {
-        for (int j = 1; j <= coloumns1; j++)
+
+        for (int j = 0; j < column; j++)
         {
-            printf("%d  ", a[i][j]);
+            printf("\nEnter Value for row %d and coloumn %d  : ", i + 1, j + 1);
+            scanf("%d", &matrix[i][j]);
         }
-        printf("\n");
     }
 }
 
-void bInput()
+// function to multiply two matrices
+void multiplyMatrices(int first[][10],
+                      int second[][10],
+                      int result[][10],
+                      int r1, int c1, int r2, int c2)
 {
-    for (int i = 1; i <= rows2; i++)
-        for (int j = 1; j <= coloumns2; j++)
+
+    // Initializing elements of matrix mult to 0.
+    for (int i = 0; i < r1; ++i)
+    {
+        for (int j = 0; j < c2; ++j)
         {
-            printf("\nEnter Value for row %d and coloumn %d  : ", i, j);
-            scanf("%d", &b[i][j]);
+            result[i][j] = 0;
         }
+    }
+
+    // Multiplying first and second matrices and storing it in result
+    for (int i = 0; i < r1; ++i)
+    {
+        for (int j = 0; j < c2; ++j)
+        {
+            for (int k = 0; k < c1; ++k)
+            {
+                result[i][j] += first[i][k] * second[k][j];
+            }
+        }
+    }
 }
 
-void bOutput()
+// function to display the matrix
+void display(int matrix[][10], int row, int column)
 {
-    for (int i = 1; i <= rows2; i++)
+    for (int i = 0; i < row; ++i)
     {
-        for (int j = 1; j <= coloumns2; j++)
+        for (int j = 0; j < column; ++j)
         {
-            printf("%d  ", b[i][j]);
+            printf("%d  ", matrix[i][j]);
         }
         printf("\n");
     }
@@ -51,63 +59,48 @@ void bOutput()
 
 int main()
 {
-    int size;
+    int first[10][10], second[10][10], result[10][10], r1, c1, r2, c2;
 
     printf("Matrix Multipication Program\n");
     printf("By Bhagya Mudgal\n");
 
     printf("\nEnter no. of rows for Matrix 1 : ");
-    scanf("%d", &rows1);
+    scanf("%d", &r1);
     printf("\nEnter no. of coloumns for Matrix 1 : ");
-    scanf("%d", &coloumns1);
+    scanf("%d", &c1);
 
     printf("\nEnter Data for Matrix 1 :\n");
-    aInput();
-    printf("\nMatrix 1 :\n");
-    aOutput();
+    // get elements of the first matrix
+    getMatrixElements(first, r1, c1);
+    printf("\nMatrix 1 :\n\n");
+    display(first, r1, c1);
 
 rerun:
     printf("\nEnter no. of rows for Matrix 2 : ");
-    scanf("%d", &rows2);
+    scanf("%d", &r2);
     printf("\nEnter no. of coloumns for Matrix 2 : ");
-    scanf("%d", &coloumns2);
+    scanf("%d", &c2);
 
-    if (coloumns1 != rows2)
+    if (c1 != r2)
     {
-        printf("\nMatrix Multipication Can Not Be Done \n<< No. of Coloumns of Matrix 1 not equal to No. of Rows of Matrix 2 >>\n<<Enter Correct Size>>\n");
+        printf("\nMatrix Multipication Can Not Be Done \n<< No. of Coloumns of Matrix 1 not equal to No. of Rows of Matrix 2 >>\n<< Enter Correct Size >>\n");
         goto rerun;
     }
     else
     {
         printf("\nEnter Data for Matrix 2 :\n");
-        bInput();
-        printf("\nMatrix 2 :\n");
-        bOutput();
-
-        for (int i = 1; i <= rows1; i++)
-        {
-            for (int j = 1; j <= coloumns2; j++)
-            {
-                for (int k = 1; i <= rows2; k++)
-                {
-                    sum = sum + a[i][k] * b[k][j];
-                }
-                ab[i][j] = sum;
-                sum = 0;
-            }
-        }
+        // get elements of the second matrix
+        getMatrixElements(second, r2, c2);
+        printf("\nMatrix 2 :\n\n");
+        display(second, r2, c2);
     }
+    printf("\nProduct Of Matrix 1 and 2 :\n\n");
 
-    printf("\nProduct Of Matrix 1 and 2 :\n");
+    // multiply two matrices.
+    multiplyMatrices(first, second, result, r1, c1, r2, c2);
 
-    for (int i = 1; i <= rows1; i++)
-    {
-        for (int j = 1; j <= coloumns2; j++)
-        {
-            printf("%d  ", ab[i][j]);
-        }
-        printf("\n");
-    }
+    // display the result
+    display(result, r1, c2);
 
     return 0;
 }
